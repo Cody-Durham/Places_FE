a3ea3815e24098c72eb8ca07aacb725bac5b76f0
Cody Durham <codydd@gmail.com>
2021-05-17 22:53:43 -0600

    BE finish. FE started. Working on App, need to finish Form and Display.Nothing to render yet.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {Route, Link, Switch} from 'react-router-dom'
+import Display from './Display'
+import Form from './Form'
 
 function App() {
+  const url = "not sure what to put here"
+
+  const [places, setPlaces] = React.useState([])
+
+  const emptyPlace = {
+    name: '', 
+    img: '', 
+    description: '',
+  }
+
+//setting new state for updating each place were going to change
+const [selectedPlace, setSelectedPlace] = React.useState(emptyPlace)
+
+
+const getPlace = () => {
+  // making a get request from this url
+  fetch(url + '/place/')
+  .then( (response) => response.json() )
+  .then( (data) => {
+    setSelectedPlace(data)
+  })
+}
+
+React.useEffect( () => {
+  getPlace()
+}, [])
+
+const handleCreate = (newPlace) => {
+  console.log('test');
+  fetch(url + '/place/', {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application.json'
+    },
+    body: JSON.stringify(newPlace)
+  })
+  .then( () => getPlace() )
+}
+
+// function to specify what place were updating (updating state here)
+const selectPlace = (place) => {
+  setSelectedPlace(place)
+}
+
+//Delete a place function
+const deletePlace = (place) => {
+  fetch(url + '/place/', {
+    method: 'delete'
+  })
+  .then( () => {
+    getPlace()
+  } )
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Find your favorite places</h1>
+      <hr/>
+      <Link>
+        <button>Add a new place</button>
+      </Link>
+      <main>
+      <Switch>
+        <Route
+            exact path="/"
+            // render={(routerProps) => (<Display {...routerProps}
+            />
+            )}
+            />
+
+        <Route
+            exact path="/create"
+            render={(routerProps) => (
+            <Form
+            {...routerProps}
+            label='create'
+            place='emptyPlace'
+            handleSubmit={handleCreate}
+            />
+            )}
+            />
+
+        <Route
+            exact path="/edit"
+            render={(routerProps) => (
+            <Form
+            {...routerProps}
+            label='create'
+            place='emptyPlace'
+            handleSubmit={selectedPlace}
+            />
+            )}
+            />
+        </Switch>
+      </main>
     </div>
   );
 }
